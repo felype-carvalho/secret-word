8c6b3d1f4b3274576865a5106c014c992a2d284f
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-02-23 17:51:36 -0300

    First commit with general adjustments

---
+++ src/data/words.js
@@ -0,0 +1,18 @@
+export const wordsListEG = {
+  car: ["Motor", "Door", "Hood", "Tire", "Antenna"],
+  fruit: ["Banana", "Apple", "Pear", "Papaya", "Orange"],
+  body: ["Arm", "Leg", "Brain", "Neck", "Eyes"],
+  computer: ["Mouse", "Keyboard", "Monitor", "Desktop"],
+  programming: ["Language", "Framework", "JavaScript", "React"],
+  food: ["Rice", "Bean", "Meat", "Milk", "Egg"],
+};
+
+export const wordsListPT = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
+
