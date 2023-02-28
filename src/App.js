4650f32cb24de1757644b6ba384b7c0a7b977d4b
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-02-28 12:25:44 -0300

    Added pick a random word and category

--- src/App.js
+++ src/App.js
@@ -22,8 +22,40 @@ function App() {
     const [gameStage, setGameStage] = useState(stages[0].name);
     const [words] = useState(wordsListEG);
 
+    const [pickedWord, setPickedWord] = useState("");
+    const [pickedCategoriy, setPickedCategoriy] = useState("");
+    const [letters, setLetters] = useState([]);
+
+    const pickWordAndCategory = () => {
+        // pick a random category
+        const categories = Object.keys(words);
+        const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+        // pick a random word
+        const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+        return { word, category }
+    }
+
     // starts the secret word game
     const startGame = () => {
+        //pick word and pick category
+        const { word, category } = pickWordAndCategory();
+
+        // create an array of letters
+        let wordLettlers = word.split("");
+
+        wordLettlers = wordLettlers.map((l) => l.toLowerCase());
+
+        console.log(word, category);
+        console.log(wordLettlers);
+
+        // fill states
+
+        setPickedWord(word);
+        setPickedCategoriy(category);
+        setLetters(letters);
+
         setGameStage(stages[1].name);
     }
 
@@ -34,14 +66,14 @@ function App() {
 
     // restarts the game
     const restartGame = () => {
-        setGameStage(stages[1].name);
+        setGameStage(stages[0].name);
     }
 
     return (
         <div className="App">
             {gameStage === 'start' && <StartScreen startGame={startGame} />}
-            {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
-            {gameStage === 'end' && <GameOver restartGame={restartGame}/>}
+            {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+            {gameStage === 'end' && <GameOver restartGame={restartGame} />}
 
         </div>
     );
