159ac7fde047c0014bb9da29f3bd856222610c43
felype-carvalho <felypecarvalho09@outlook.com.br>
2023-02-28 11:11:26 -0300

    Create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+//CSS
 import './App.css';
+
+//React
+import { useCallback, useEffect, useState } from "react";
+
+//Data
+import { wordsListPT } from './data/words';
+import { wordsListEG } from './data/words';
+
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+    { id: 1, name: "start" },
+    { id: 2, name: "game" },
+    { id: 3, name: "end" }
+];
 
 function App() {
+    const [gameStage, setGameStage] = useState(stages[0].name);
+    const [words] = useState(wordsListEG);
+
+    console.log(words);
     return (
         <div className="App">
-            <StartScreen />
+            {gameStage === 'start' && <StartScreen />}
+            {gameStage === 'game' && <Game />}
+            {gameStage === 'end' && <GameOver />}
+
         </div>
     );
 }
